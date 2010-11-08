411ae0621633bf1f93459ad35afb55fbe38bcbca
justinkamerman <justinkamerman@5f0f0774-7a76-4a66-a963-dbef20c5c3ac>
2010-11-08 20:44:54 +0000

    Added Windows driver script, UML diagram, logs

---
+++ mldm/prog2/run.bat
@@ -0,0 +1,21 @@
+rem  
+rem  $Id: run.sh 17 2010-08-02 20:05:09Z justin.kamerman $ 
+rem  
+rem  $LastChangedDate: 2010-08-02 17:05:09 -0300 (Mon, 02 Aug 2010) $ 
+rem  
+rem  $LastChangedBy: justin.kamerman $ 
+rem  
+rem  car.data:        run.sh -f data\car.data -n buying,maint,doors,persons,lug_boot,safety
+rem 
+rem  ecoli.data:      run.sh -f data\ecoli.data -n mcg,gvh,lip,chg,aac,alm1,alm2
+rem 
+rem  breast-cancer-wisconsin.data: run.sh -f data\breast-cancer-wisconsin.data -n Clump_Thickness,Uniformity_of_Cell_Size,Uniformity_of_Cell_Shape,Marginal_Adhesion,Single_Epithelial_Cell_Size,Bare_Nuclei,Bland_Chromatin,Normal_Nucleoli,Mitoses
+rem 
+rem  letter-recognition:   run.sh -f data\letter-recognition.data -n x-box,y-box,width,high,onpix,x-bar,y-bar,x2bar,y2bar,xybar,x2ybr,xy2br,x-ege,xegvy,y-ege,yegvx
+rem 
+rem  mushroom.data:        run.sh -f data\mushroom.data -n cap-shape,cap-surface,cap-color,bruises?,odor,gill-attachment,gill-spacing,gill-size,gill-color,stalk-shape,stalk-root,stalk-surface-above-ring,stalk-surface-below-ring,stalk-color-above-ring,stalk-color-below-ring,veil-type,veil-color,ring-number,ring-type,spore-print-color,population,habitat
+rem 
+
+@echo off
+java -cp dist\mldm.jar;thirdparty\commons-cli-1.2.jar -Djava.util.logging.config.file=logging.properties Main %*
+
